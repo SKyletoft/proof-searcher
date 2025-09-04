02e9b11a83094a3f67418c009d75449815a02546
Samuel Kyletoft <samuel@kyletoft.se>
2025-09-04 04:20:34 +0200

    flake: Switch to stable rust + BOOTSTRAP var

--- flake.nix
+++ flake.nix
@@ -2,29 +2,31 @@
 	inputs = {
 		nixpkgs.url      = "github:nixos/nixpkgs/nixpkgs-unstable";
 		flake-utils.url  = "github:numtide/flake-utils";
-		rust-overlay.url = "github:oxalica/rust-overlay";
 	};
 
-	outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+	outputs = { self, nixpkgs, flake-utils }:
 		flake-utils.lib.eachDefaultSystem(system:
 			let
 				pkgs = import nixpkgs {
 					inherit system;
-					overlays = [ (import rust-overlay) ];
 				};
-				toolchain = pkgs.rust-bin.nightly.latest.default;
 			in {
-				packages.default = (pkgs.makeRustPlatform {
-					cargo = toolchain;
-					rustc = toolchain;
-				}).buildRustPackage {
+				packages.default = pkgs.rustPlatform.buildRustPackage {
 					pname = "proof-search";
 					version = "0.0.1";
 					src = ./.;
 					cargoLock.lockFile = ./Cargo.lock;
 				};
 				devShells.default = pkgs.mkShell {
-					nativeBuildInputs = [ toolchain ];
+					RUSTC_BOOTSTRAP = "1";
+					nativeBuildInputs = with pkgs; [
+						cargo
+						rustc
+						rustfmt
+						clippy
+						rust-analyzer
+						gdb
+					];
 				};
 			}
 		);
